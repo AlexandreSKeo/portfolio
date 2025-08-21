1c4fd17a41f5959403625ca7b1715b16553fc6c1
CodeMarrow <alexandreskeo90@gmail.com>
2025-08-20 23:36:53 -0400

    feat: Top nav bar with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <header>
+        <div className="flex gap-4">
+          <Link href="https://github.com/alexandreskeo">Github</Link>
+          <Link href="https://www.linkedin.com/in/alexandreskeo/">LinkedIn</Link>
+          <Link href="https://x.com/alexandreskeo">X</Link>
+          <Link href="mailto:alexandrekeo@icloud.com">Contact</Link>
+        </div>
+      </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
