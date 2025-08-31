f253390e662bfc9209a014f800da9a061ad88512
CodeMarrow <alexandreskeo90@gmail.com>
2025-08-30 21:05:41 -0400

    feat: Add gradient background on full page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <header>
-        <div className="flex gap-4">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 mesh-gradient">
+      <header className="w-full">
+        <div className="flex gap-4 justify-center">
           <Link href="https://github.com/alexandreskeo">Github</Link>
           <Link href="https://www.linkedin.com/in/alexandreskeo/">LinkedIn</Link>
           <Link href="https://x.com/alexandreskeo">X</Link>
           <Link href="mailto:alexandrekeo@icloud.com">Contact</Link>
         </div>
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <h1>Welcome</h1>
-        <p>I&apos;m a software developer with a passion for building projects.</p>
+      <main className="flex flex-col items-center w-full">
+        <div className="w-11/12 max-w-xl p-20 text-center text-white backdrop-blur-sm border border-[var(--border-subtle)] rounded-md shadow-xl">
+          <h1>Welcome</h1>
+          <p>I&apos;m a software developer with a passion for building projects.</p>
+        </div>
       </main>
     </div>
   );
